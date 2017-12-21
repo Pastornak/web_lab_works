d065513931167ccb7365ffa3e5fa32bc89f90829
Yurii Pasternak <yurapaster@gmail.com>
2017-12-21 23:59:34 +0200

    Added all pages routes

--- lab_10_11/app/routes/page_routes.js
+++ lab_10_11/app/routes/page_routes.js
@@ -4,16 +4,22 @@ module.exports = function(app, db) {
   app.get('/', (req, res) => {
     res.sendFile(_dirname + '/Main.html');
   });
-  app.get('/Main.html', (req, res) => {
-    res.sendFile(_dirname + '/Main.html');
+  app.get('/Admin.html', (req, res) => {
+    res.sendFile(_dirname + '/Admin.html');
+  });
+  app.get('/Contacts.html', (req, res) => {
+    res.sendFile(_dirname + '/Contacts.html');
   });
   app.get('/Feedback.html', (req, res) => {
     res.sendFile(_dirname + '/Feedback.html');
   });
+  app.get('/Main.html', (req, res) => {
+    res.sendFile(_dirname + '/Main.html');
+  });
   app.get('/News.html', (req, res) => {
     res.sendFile(_dirname + '/News.html');
   });
-  app.get('/Admin.html', (req, res) => {
-    res.sendFile(_dirname + '/Admin.html');
+  app.get('/Study_plan.html', (req, res) => {
+    res.sendFile(_dirname + '/Study_plan.html');
   });
 };
